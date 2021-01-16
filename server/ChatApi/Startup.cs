e4b7571f405a3474552d4fd6ba86d583ce1f32d1
YoussefMouad <imouad.you@gmail.com>
2021-01-16 19:11:44 +0100

    Feat: serve client files from the server

--- server/ChatApi/Startup.cs
+++ server/ChatApi/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ using ChatApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
@@ -86,6 +90,12 @@ namespace ChatApi
                 };
             });
 
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
             services.AddSignalR();
         }
 
@@ -98,7 +108,7 @@ namespace ChatApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChatApi v1"));
             }
-            
+
             app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
@@ -113,6 +123,18 @@ namespace ChatApi
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chathub");
+            });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"ClientApp/dist")),
+                RequestPath = new PathString("")
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
             });
         }
     }
